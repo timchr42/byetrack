a3c1c009545f46997b18736b35a0cea89f91d32f
timchr42 <timjannis03@gmail.com>
2025-09-20 18:44:52 +0200

    compileSdk fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "de.cispa.byetrackappside"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "de.cispa.byetrackappside"
--- byetrack/build.gradle.kts
+++ byetrack/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "de.cispa.byetrack"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 34
